48b677286076d8a21fcf884a051d3c3b6b7b1ef5
jiaopeng1 <893277858@qq.com>
2022-04-05 15:37:05 +0800

    更新

--- TwoPointers.cpp
+++ TwoPointers.cpp
@@ -473,6 +473,21 @@ public:
  	}
 
 	// 单调队列
+	/*
+	输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+	输出: [3,3,5,5,6,7]
+
+	解释过程中队列中都是具体的值，方便理解，具体见代码。
+	初始状态：L=R=0,队列:{}
+	i=0,nums[0]=1。队列为空,直接加入。队列：{1}
+	i=1,nums[1]=3。队尾值为1，3>1，弹出队尾值，加入3。队列：{3}
+	i=2,nums[2]=-1。队尾值为3，-1<3，直接加入。队列：{3,-1}。此时窗口已经形成，L=0,R=2，result=[3]
+	i=3,nums[3]=-3。队尾值为-1，-3<-1，直接加入。队列：{3,-1,-3}。队首3对应的下标为1，L=1,R=3，有效。result=[3,3]
+	i=4,nums[4]=5。队尾值为-3，5>-3，依次弹出后加入。队列：{5}。此时L=2,R=4，有效。result=[3,3,5]
+	i=5,nums[5]=3。队尾值为5，3<5，直接加入。队列：{5,3}。此时L=3,R=5，有效。result=[3,3,5,5]
+	i=6,nums[6]=6。队尾值为3，6>3，依次弹出后加入。队列：{6}。此时L=4,R=6，有效。result=[3,3,5,5,6]
+	i=7,nums[7]=7。队尾值为6，7>6，弹出队尾值后加入。队列：{7}。此时L=5,R=7，有效。result=[3,3,5,5,6,7]
+	*/
 	vector<int> maxSlidingWindow1(vector<int>& nums, int k) {
 		int n = nums.size();
 		deque<int> q;
@@ -491,6 +506,12 @@ public:
 			}
 
 			q.emplace_back(i);
+
+			/*
+				当这个最大值不在滑动窗口中时, 将其弹出队列。 如：2, 1, -2, 1
+				当前队列头元素的下标是0, 说明当前入队列的元素不是最大的. 此时需要
+				将队头元素弹出. 剩下的元素中最大的就是当前窗口中最大元素.
+			*/
 			while (q.front() <= i - k) {
 				q.pop_front();
 			}
@@ -539,11 +560,42 @@ int main() {
 	// SolutionCheckInclusion s;
 	// cout << s.checkInclusion("ab", "eidbaooo") << endl;
 
-	// lc 632
+	// lc 632 给你k个数组，找一个最小区间[a,b]，可以包含k个数组中的数字各至少一个。
 
 	// lc 727
+	/*
+	Given strings S and T, find the minimum (contiguous) substring W of S, so that T is a subsequenceof W.
+	If there is no such window in S that covers all characters in T, return the empty string "". If there 
+	are multiple such minimum-length windows, return the one with the left-most starting index.
+
+	Example 1:
+	Input: 
+	S = "abcdebdde", T = "bde"
+	Output: "bcde"
+	Explanation: 
+	"bcde" is the answer because it occurs before "bdde" which has the same length.
+	"deb" is not a smaller window because the elements of T in the window must occur in order.
+
+	Note:
+	All the strings in the input will only contain lowercase letters.
+	The length of S will be in the range [1, 20000].
+	The length of T will be in the range [1, 100].
+	Runtime: 44 ms, faster than 73.35% of C++ online submissions for Minimum Window Subsequence.
+	*/
 
 	// lc 159
+	/*
+	给定一个字符串 s ，找出 至多 包含两个不同字符的最长子串 t 。
+	示例 1:
+	输入: “eceba”
+	输出: 3
+	解释: t 是 “ece”，长度为3。
+
+	示例 2:
+	输入: “ccaabbb”
+	输出: 5
+	解释: t 是 “aabbb”，长度为5。
+	*/
 
 	// lc 239 O(n*k)需要优化
 	// 思路1：维护一个大根堆, 窗口移动的时候
